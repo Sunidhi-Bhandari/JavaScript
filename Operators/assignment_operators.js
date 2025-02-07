2d6f21893ea5df2a4f2f3014aacbc4f8672bc426
Sunidhi Bhandari <sunidhibhandarisep1@gmail.com>
2025-02-07 14:56:59 +0530

    Arithmetic Operators

---
+++ Operators/assignment_operators.js
@@ -0,0 +1,17 @@
+//Assugnment Operators
+// 1. = --> equals to 
+// 2. += --> add and equals to
+// 3. -= --> sub and equala to
+// 4. /= --> divide and equals to 
+// 5. *= --> multiply and equals to
+// 6. %= --> mod and equals to
+// 7. **= --> to the power and equals to 
+
+let a = 10 
+
+console.log('a+= is ', a+= 1) //11
+console.log('a-= is', a-= 1) //10
+console.log('a/= is', a/=2) //5
+console.log('a*= is', a*=2) //10
+console.log('a**= is', a**=2) //100
+console.log('a%=2', a%=2) //0
