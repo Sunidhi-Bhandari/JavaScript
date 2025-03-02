fee1a39b95a762f59d1c6cfa15875c2828fb29c1
Sunidhi Bhandari <sunidhibhandari10@gamil.com>
2025-03-02 22:32:51 +0530

    Strings

---
+++ Strings/Template_Literals.js
@@ -0,0 +1,7 @@
+// Template literals are `` we can use backtick to insert a string and a value 
+// Template literals are also known as String Interpolination
+
+let girl = 'Mary'
+let boy = 'Mark'
+
+console.log(`Girl is ${girl} and boy is ${boy}`)
